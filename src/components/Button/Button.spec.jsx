e7a07a6cd565d22a65051a75725b14319b7d22cd
vinicius4006 <vfranca477@gmail.com>
2022-03-09 09:28:41 -0300

    ajustes-testes

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { click } from '@testing-library/user-event/dist/click';
 import { Button } from '.';
 
 describe('<Button />', () => {
@@ -9,5 +10,35 @@ describe('<Button />', () => {
 
         const button = screen.getByRole('button', { name: /load more +/i });
         expect(button).toHaveAttribute('class', 'btn btn-transition btn-styles');
+    });
+
+    it('should call function on button click', () => {
+
+        const fn = jest.fn();
+        render(<Button text="Load More +" loadMoreDigi={fn} />);
+
+        const button = screen.getByRole('button', { name: /load more +/i });
+
+        click(button);
+        //fireEvent.click(button);
+        expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    it('should be disabled when disabled is true', () => {
+
+        render(<Button text="Load More +" notMoreDigi={true} />);
+
+        const button = screen.getByRole('button', { name: /load more +/i });
+
+        expect(button).toBeDisabled();
+    });
+
+    it('should be enabled when disabled is false', () => {
+
+        render(<Button text="Load More +" notMoreDigi={false} />);
+
+        const button = screen.getByRole('button', { name: /load more +/i });
+
+        expect(button).toBeEnabled();
     });
 });
